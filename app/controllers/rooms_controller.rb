addec23ee8ea1feb40836d7924f4bc908c7eec66
agung-w <suronokz3471@yahoo.com>
2024-04-26 23:04:59 +0700

    Add show room and fix styling issues

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -21,6 +21,11 @@ class RoomsController < ApplicationController
   def edit
   end
 
+  def show
+    @room_message = RoomMessage.new room: @room
+    @room_messages = @room.room_messages
+  end
+  
   def update
     if @room.update_attributes(permitted_parameters)
       flash[:success] = "Room #{@room.name} was updated successfully"
